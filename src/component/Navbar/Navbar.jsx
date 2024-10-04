b76ce7ad2420f3709c6434cfa3923aab504a96df
jashan22a <taruna.webexpert@gmail.com>
2024-10-04 12:08:10 +0530

    Update the navbar code

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,135 +1,206 @@
-"use client";
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import Link from "next/link";
-import { useSession } from "next-auth/react";
-import { routesUrl } from "@/utils/pagesurl";
-import LogoutButton from "../shared/form/LogoutButton";
-import { usePathname } from "next/navigation";
-import { useState } from "react";
-
+'use client'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import { useSession } from 'next-auth/react';
+import { usePathname } from 'next/navigation';
+import { routesUrl } from '@/utils/pagesurl';
+import Link from 'next/link';
+import LogoutButton from '../shared/form/LogoutButton';
+// const pages = ['Products', 'Pricing', 'Blog'];
+// const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 function ResponsiveAppBar() {
   const pathName = usePathname();
-  const [anchorElNav, setAnchorElNav] = useState(null);
-  const [anchorElUser, setAnchorElUser] = useState(null);
-  const { data: session } = useSession();
-
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const { data: session, status } = useSession(); // Get session and status
   if (pathName === "/auth/signin") {
-    return null
+    return null;
   }
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
-
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
-
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
-
+console.log("session",session)
   return (
-    <AppBar position="static" className="bg-transparent shadow-none">
+    <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
-              onClick={handleCloseNavMenu}
-              sx={{ my: 2, color: "white" }}
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
             >
-              <Link href="/">
-                <Typography>Home</Typography>
-              </Link>
+              <MenuIcon />
             </IconButton>
-            <IconButton
-              onClick={handleCloseNavMenu}
-              sx={{ my: 2, color: "white" }}
-            >
-              <Link href={routesUrl.about}>
-                <Typography>About</Typography>
-              </Link>
-            </IconButton>
-            {session ? (
-              <div>
-                <IconButton
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: "white" }}
-                >
-                  <Link href={routesUrl.products}>
-                    <Typography>Products</Typography>
-                  </Link>
-                </IconButton>
-                <IconButton
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: "white" }}
-                >
-                  <Link href={routesUrl.allUser}>
-                    <Typography>All User</Typography>
-                  </Link>
-                </IconButton>
-              </div>
-            ) : (""
-            )}
-          </Box>
-          <Box>{session?.user?.email}</Box>
-          <Box sx={{ flexGrow: 0, marginLeft:2}}>
-            <Tooltip title={session ? "Account settings" : ""}>
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                {session ? (
-                  <Avatar src={session.user?.dummyImage} />
-                ) : (
-                  <Link href={routesUrl.signIn}>
-                    <Typography className="signin-button" color="white">Sign In</Typography>
-                  </Link>
-                )}
-              </IconButton>
-            </Tooltip>
             <Menu
-              sx={{ mt: "45px" }}
               id="menu-appbar"
-              anchorEl={anchorElUser}
+              anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
+                vertical: 'bottom',
+                horizontal: 'left',
               }}
               keepMounted
               transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
+                vertical: 'top',
+                horizontal: 'left',
               }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: 'block', md: 'none' } }}
             >
-              {session ? (
-                <div>
-                  <MenuItem onClick={handleCloseUserMenu}>
-                    <Link href={routesUrl.allUser}>
-                      <Typography textAlign="center">User</Typography>
-                    </Link>
-                  </MenuItem>
-                  <MenuItem>
-                    <div>
-                      <LogoutButton />
-                    </div>
-                  </MenuItem>
-                </div>
-              ) : null}
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography sx={{ textAlign: 'center' }}>Home</Typography>
+                <Typography sx={{ textAlign: 'center' }}>About</Typography>
+                <Typography sx={{ textAlign: 'center' }}>Products</Typography>
+                <Typography sx={{ textAlign: 'center' }}>All user</Typography>
+              </MenuItem>
             </Menu>
           </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            <Link href="/">
+              <Button
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                Home
+              </Button>
+            </Link>
+            <Link href={routesUrl.about}>
+              <Button
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                About
+              </Button>
+            </Link>
+            {session ? (
+              <>
+                <Link href={routesUrl.products}>
+                  <Button
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                  >
+                    Products
+                  </Button>
+                </Link>
+                <Link href={routesUrl.allUser}>
+                  <Button
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: 'white', display: 'block' }}
+                  >
+                    All User
+                  </Button>
+                </Link>
+              </>
+            ) : (
+              ""
+            )}
+          </Box>
+          {session ? (
+            <>
+            {session.user.email}
+            <Box sx={{ flexGrow: 0 ,marginLeft:3} }>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+                >
+                 <MenuItem onClick={handleCloseUserMenu}>
+                  <Typography sx={{ textAlign: 'center' }}>User</Typography>
+                </MenuItem>
+                <MenuItem onClick={handleCloseUserMenu}>
+                  <Typography sx={{ textAlign: 'center' }}><LogoutButton /></Typography>
+                </MenuItem>
+              </Menu>
+            </Box>
+                </>
+          ) : (
+            <Link href={routesUrl.signIn}>
+              <Typography className="signin-button" color="white">
+                Sign In
+              </Typography>
+            </Link>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
   );
 }
-
 export default ResponsiveAppBar;
